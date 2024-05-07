f491f09568d7cde03f2661dddf7d551942737c38
Pranto Mollick <prantocse01@gmail.com>
2024-05-07 15:39:46 +0600

    footer component design complete in tailwinds

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,4 +1,6 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -6,9 +8,11 @@ type LayoutProps = {
 
 function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Header />
+      <Hero />
       <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
     </div>
   );
 }
