5c3935ee3a09a8d50482dcc05aa418dc3581d255
Pranto Mollick <prantocse01@gmail.com>
2024-05-13 13:49:18 +0600

    new toast message component included with mutation function into my custom hook useUpdateMyUser

--- frontend/src/api/useUpdateMyUser.ts
+++ frontend/src/api/useUpdateMyUser.ts
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
 
@@ -35,15 +36,22 @@ export const useUpdateMyUser = () => {
 
   const {
     mutateAsync: updateUser,
-    isError,
+    error,
+    reset,
     isLoading,
     isSuccess,
   } = useMutation(updateMyUserRequest);
 
+  if (isSuccess) {
+    toast.success("User profile updated!");
+  }
+
+  if (error) {
+    toast.error(error.toString());
+    reset();
+  }
   return {
     updateUser,
-    isError,
     isLoading,
-    isSuccess,
   };
 };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,7 @@ import AppRoutes from "./AppRoutes.tsx";
 import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.tsx";
 
 import "./global.css";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,6 +24,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <Auth0ProviderWithNavigate>
           <AppRoutes />
+          <Toaster visibleToasts={1} position="top-right" richColors />
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
